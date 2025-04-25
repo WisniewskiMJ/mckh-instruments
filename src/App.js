cc2122e0f36a125d7a1b2d9a64587c45d73f09d2
WisniewskiMJ <bakshee.jw@gmail.com>
2025-04-25 23:58:33 +0200

    Refactor category-list component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,36 @@
 import CategoryList from "./components/category-list/category-list.component";
 
-const App = () => <CategoryList />;
+const App = () => {
+
+  const categories = [
+    {
+      id: 1,
+      name: "strings",
+      imageUrl: "/images/strings.jpg",
+    },
+    {
+      id: 2,
+      name: "brass",
+      imageUrl: "/images/brass.jpg",
+    },
+    {
+      id: 3,
+      name: "woodwind",
+      imageUrl: "/images/woodwind.jpg",
+    },
+    {
+      id: 4,
+      name: "keys",
+      imageUrl: "/images/keys.jpg",
+    },
+    {
+      id: 5,
+      name: "percussion",
+      imageUrl: "/images/percussion.jpg",
+    },
+  ];
+
+  return <CategoryList categories={categories} />;
+};
 
 export default App;
--- src/components/category-list/category-list.component.jsx
+++ src/components/category-list/category-list.component.jsx
@@ -1,42 +1,12 @@
 import CategoryItem from "../category-item/category-item.component";
 import "./category-list.styles.scss";
 
-const CategoryList = () => {
-	const categories = [
-		{
-			id: 1,
-			name: "strings",
-			imageUrl: "/images/strings.jpg",
-		},
-		{
-			id: 2,
-			name: "brass",
-			imageUrl: "/images/brass.jpg",
-		},
-		{
-			id: 3,
-			name: "woodwind",
-			imageUrl: "/images/woodwind.jpg",
-		},
-		{
-			id: 4,
-			name: "keys",
-			imageUrl: "/images/keys.jpg",
-		},
-		{
-			id: 5,
-			name: "percussion",
-			imageUrl: "/images/percussion.jpg",
-		},
-	];
-
-	return (
-		<div className='category-list-container'>
-			{categories.map((category) => (
-				<CategoryItem key={category.id} category={category} />
-			))}
-		</div>
-	);
-};
+const CategoryList = ({ categories }) => (
+	<div className='category-list-container'>
+		{categories.map((category) => (
+			<CategoryItem key={category.id} category={category} />
+		))}
+	</div>
+);
 
 export default CategoryList;
