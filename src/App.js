8b1623d99bb960c56158c512be79fdc18eaf98ff
WisniewskiMJ <bakshee.jw@gmail.com>
2025-04-25 19:10:29 +0200

    Make category list and item components

--- src/App.js
+++ src/App.js
@@ -1,50 +1,5 @@
-import './categories.styles.scss';
+import CategoryList from "./components/category-list/category-list.component";
 
-const App = () => {
-
-  const categories = [
-    {
-      id: 1,
-      name: 'strings',
-      imageUrl: '/images/strings.jpg'
-    },
-    {
-      id: 2,
-      name: 'brass',
-      imageUrl: '/images/brass.jpg'
-    },
-    {
-      id: 3,
-      name: 'woodwind',
-      imageUrl: '/images/woodwind.jpg'
-    },
-    {
-      id: 4,
-      name: 'keys',
-      imageUrl: '/images/keys.jpg'
-    },
-    {
-      id: 5,
-      name: 'percussion',
-      imageUrl: '/images/percussion.jpg'
-    }
-  ];
-
-  return (
-    <div className='categories-container'>
-      { categories.map(({ id, name, imageUrl }) => (
-        <div key={ id } className='category-container'>
-          <div className='background-image' style={{
-            backgroundImage: `url(${imageUrl})`
-          }} />
-          <div className='category-body-container'>
-            <h2>{ name }</h2>
-            <p>Shop Now</p>
-          </div>
-        </div>
-      ))}
-    </div>
-  );
-};
+const App = () => <CategoryList />;
 
 export default App;
