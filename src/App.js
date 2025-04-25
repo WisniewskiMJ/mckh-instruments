12e32abb3078ad4c4da598c511450c1f4b3e05b5
WisniewskiMJ <bakshee.jw@gmail.com>
2025-04-25 17:00:13 +0200

    Create initial App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,41 @@
-const App = () => (
-  <div>App</div>
-);
+const App = () => {
+
+  const categories = [
+    {
+      id: 1,
+      name: 'Strings'
+    },
+    {
+      id: 2,
+      name: 'Brass'
+    },
+    {
+      id: 3,
+      name: 'Woodwind'
+    },
+    {
+      id: 4,
+      name: 'Keys'
+    },
+    {
+      id: 5,
+      name: 'Percussion'
+    }
+  ];
+
+  return (
+    <div className='categories-container'>
+      { categories.map(({ id, name }) => (
+        <div className='category-container' key={ id }>
+          {/* <img /> */}
+          <div className='category-body-container'>
+            <h2>{ name }</h2>
+            <p>Shop Now</p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export default App;
