cd9fd7754216acb67bfc3ff4426a46d1f2a02b14
WisniewskiMJ <bakshee.jw@gmail.com>
2025-04-26 19:37:55 +0200

    Add navigation bar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import { Routes, Route } from 'react-router-dom';
 
+import NavBar from './routes/nav-bar/nav-bar.component';
 import Home from './routes/home/home.component'
+
+const Shop = () => (
+  <h1>Shop</h1>
+);
 
 const App = () => (
   <Routes>
-    <Route path='/' element={<Home />} />
-  </Routes>
+    <Route path='/' element={<NavBar />}>
+      <Route index element={<Home />} />
+      <Route path='/shop' element={<Shop />} />
+    </Route>
+  </Routes >
 );
 
 export default App;
---
+++ src/routes/nav-bar/nav-bar.component.jsx
@@ -0,0 +1,26 @@
+import { Fragment } from "react";
+import { Outlet, Link } from "react-router-dom";
+
+import { ReactComponent as MckhLogo } from "../../assets/crown.svg";
+
+import "./nav-bar.styles.scss";
+
+const NavBar = () => {
+	return (
+		<Fragment>
+			<div className='nav-bar'>
+				<Link className='logo-container' to='/'>
+					<MckhLogo className='logo' />
+				</Link>
+				<div className='nav-links-container'>
+					<Link className='nav-link' to='/shop'>
+						SHOP
+					</Link>
+				</div>
+			</div>
+			<Outlet />
+		</Fragment>
+	);
+};
+
+export default NavBar;
